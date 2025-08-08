fcb17e6eca924270ab8883f6d748aa0c02893bc4
PrasadGhadge2211 <prasad.ghadge1986@gmail.com>
2025-08-08 14:58:30 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, jsonify
-from datetime import datetime, timedelta
-from pymongo import MongoClient, ASCENDING, DESCENDING
+from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
-import certifi
-import pytz
 
 load_dotenv()
-ca = certifi.where()
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 MONGO_URI = os.environ.get("MONGODB_URI", "")
-client = MongoClient(MONGO_URI, tlsCAFile=ca)
+client = MongoClient(MONGO_URI)
 db = client['pharmacy_db']
-LOCAL_TIMEZONE = pytz.timezone('Asia/Kolkata')
-
-@app.template_filter('local_datetime')
-def local_datetime_filter(dt):
-    if isinstance(dt, str):
-        dt = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-    if dt.tzinfo is None:
-        dt = pytz.utc.localize(dt)
-    return dt.astimezone(LOCAL_TIMEZONE).strftime('%Y-%m-%d %H:%M:%S')
 
 @app.route('/')
 def dashboard():

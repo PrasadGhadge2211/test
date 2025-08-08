525096fbe176ef5d00a8565e09918548c4659741
PrasadGhadge2211 <prasad.ghadge1986@gmail.com>
2025-08-08 15:00:33 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, jsonify
-from pymongo import MongoClient
-import os
 
-load_dotenv()
 app = Flask(__name__)
-MONGO_URI = os.environ.get("MONGODB_URI", "")
-client = MongoClient(MONGO_URI)
-db = client['pharmacy_db']
 
 @app.route('/')
 def dashboard():
